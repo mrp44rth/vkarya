27d2cf7743bf5340baf57e7284f9dd70fc2d69eb
mrp44rth <113108394+mrp44rth@users.noreply.github.com>
2022-12-22 13:45:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
        /*===== Resize Navbar on Scroll =====*/
-       var navbar = document.querySelector(".navbar");
+      let docTitle = document.title;
+
+window.addEventListener("blur", (){
+
+document.title "Please Come back :(";
+
+})
+
+window.addEventListener("focus", (){
+document.title docTitle;
+
+})
+     var navbar = document.querySelector(".navbar");
        // when the scroll is higher than 20 viewport height, add the sticky classs to the tag with a class navbar 
        window.onscroll = () =>{
        this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
